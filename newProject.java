2197d400ae75cd43415b3994517d2e42cd68a766
AntonMolchanov <a.molchanov886@gmail.com>
2023-06-17 09:22:28 +0300

    add lection 1

---
+++ newProject.java
@@ -0,0 +1,11 @@
+public class newProject {
+
+    public static void main (String [] args) {
+        short age;
+        int salary;
+        age = 36;
+        salary = 130000;
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
