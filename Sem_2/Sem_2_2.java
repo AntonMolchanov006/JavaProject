f2af9641bba38a8731cdee966147d359adb0841f
AntonMolchanov <a.molchanov886@gmail.com>
2023-06-17 13:29:40 +0300

    add seminar 2 and text file

--- Sem_2/Sem_2_2.java
+++ Sem_2/Sem_2_2.java
@@ -1,7 +1,25 @@
 package Sem_2;
 
+import java.io.FileWriter;
+
 public class Sem_2_2 {
     public static void main(String[] args) {
+        // Записать в файл 10 раз слово тест
+
+        int n = 10;
+        String str = "TEST";
+
+        try{
+        FileWriter file = new FileWriter("test.txt");
+        for (int i = 0; i < n; i++) {
+            file.write(str+"\n");
+        }
+        file.close();
+        System.out.println("OK");
+        }
+        catch(Exception e){
+            System.out.println("что-то пошло не так.");
+        }
         
     }
 }
