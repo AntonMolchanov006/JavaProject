052f0dea97412c953db8b30643c6ed4f6ee4d56e
AntonMolchanov <a.molchanov886@gmail.com>
2023-06-21 23:39:21 +0300

    sem 3 begin

--- HW_2/Task_2.java
+++ HW_2/Task_2.java
@@ -2,17 +2,27 @@ package HW_2;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Date;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-import javax.sound.midi.MidiDevice.Info;
-
 public class Task_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Реализуйте алгоритм сортировки пузырьком числового массива, 
         //результат после каждой итерации запишите в лог-файл.
+            Logger ll = Logger.getLogger(Task_2.class.getName());
+            String logPath = "log.txt";
+            FileHandler fh = new FileHandler(logPath, true);
+            FileHandler fh1 = new FileHandler("logsPath.txt", true);
+            ll.addHandler(fh);
+            ll.addHandler(fh1);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            fh1.setFormatter(formatter);
+        
+
+        
 
         int [] arr = {3,4,9,6,7,0,2,8,1,5};
         int temp;
@@ -31,6 +41,7 @@ public class Task_2 {
                         arr[j+1]=temp;
                         logger = date + " index of element " + arr[j] + " became " + (j+1);
                         fw.write(logger + "\n");
+                        ll.log(Level.INFO, date + " index of element " + arr[j] + " became " + (j+1));
                     }
                 }
             }
@@ -44,5 +55,7 @@ public class Task_2 {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
         }
+        }
+        
     }
-}
+
