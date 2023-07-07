52efde8c33c0063bdca6a54de2246d14fea7c360
AntonMolchanov <a.molchanov886@gmail.com>
2023-07-07 09:50:06 +0300

    add hw 6

---
+++ HW_6/Notebook.java
@@ -0,0 +1,14 @@
+package HW_6;
+
+public class Notebook {
+    int ozu;
+    int jd;
+    String OC;
+    String color;
+
+    public void GetData() {
+        System.out.printf("ОЗУ - %d\nОбъём ЖД - %d\nОперационная система - %s\nЦвет - %s\n\n", ozu, jd, OC, color);
+    }
+}
+    
+
---
+++ HW_6/NotebookFilter.java
@@ -0,0 +1,8 @@
+package HW_6;
+
+public class NotebookFilter {
+    public static void main(String[] args) {
+        var nt1 = new Notebook();
+
+    }
+}
